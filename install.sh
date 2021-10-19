0fc2d99a8619def32de3c632c67213acc5828cb8
Aleksey Vedin <lumenlawliet@gmail.com>
2021-10-19 15:45:51 +0400

    add Jenkinsfile version 1.7

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-"""
+
 FILE=venv/
 
-if [ ! -d #$FILE ]; then
+if [ ! -d "$FILE" ]; then
     python -m venv venv
 fi
 
 source venv/bin/activate &&
 pip install -U pip && pip install -r requirements.txt
-"""
-docker build -t tests .
-
-docker run tests --url ${APP_URL} --executor ${EXECUTOR} --browser ${BROWSER} --alluredir allure-results
-
-docker system prune -f
